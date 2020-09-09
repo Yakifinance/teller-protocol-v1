05f65d6a8d36b9ac1cfbcb0d0b30135037f56e44
Noah Passalacqua <noah.passa@gmail.com>
2020-09-09 11:23:14 -0500

    Auto save Teller contract addresses in netowrk config files.

--- migrations/utils/DeployerApp.js
+++ migrations/utils/DeployerApp.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const fs = require('fs');
 const jsonfile = require('jsonfile');
 const MOCK_NETWORKS = ["test", "ganache", "soliditycoverage"];
 
@@ -145,11 +146,15 @@ DeployerApp.prototype.writeJson = function(outputJson = `./build/${this.network}
     if(this.canDeployMock()) {
         return;
     }
+
+    const tellerContracts = {}
     const jsonData = {
         contracts: []
     };
 
     for (const contractInfo of this.contracts) {
+        tellerContracts[contractInfo.name] = contractInfo.address
+
         jsonData.contracts.push({
             order: this.contracts.indexOf(contractInfo) + 1,
             address: contractInfo.address,
@@ -157,11 +162,13 @@ DeployerApp.prototype.writeJson = function(outputJson = `./build/${this.network}
         });
     }
 
+    fs.writeFileSync(`./config/networks/${this.network}/teller.json`, JSON.stringify(tellerContracts, null, 2))
+
     jsonfile.writeFile(outputJson, jsonData, {spaces: 4, EOL: '\r\n'}, function (err) {
-      console.log(`JSON file created at '${outputJson}'.`);
-      if(err) {
-        console.error("Errors: " + err);
-      }
+        console.log(`JSON file created at '${outputJson}'.`);
+        if(err) {
+            console.error("Errors: " + err);
+        }
     });
 }
 
